1c8cd8795332c240cb6f81b1d34a4c227a797432
kaustubhh123 <124804584+kaustubhh123@users.noreply.github.com>
2023-04-03 10:48:45 +0530

    Add files via upload

---
+++ InvertedHalfPyramid.java
@@ -0,0 +1,16 @@
+public class InvertedHalfPyramid
+{
+	public static void main(String[] args)
+	{
+		int i,j;
+		for(i=5;i>=1;i--)
+		{
+			for(j=1;j<=i;j++)
+			{
+				System.out.print("#");
+			}
+			System.out.println();
+
+		}
+	}
+}
